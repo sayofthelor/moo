805054aa96c0827cb7260a522ab4223633881b81
Sailor / Sabine <justsailor06@gmail.com>
2022-03-23 15:06:49 -0400

    Moooooooooooooooo.

---
+++ moo/Moo.hx
@@ -0,0 +1,7 @@
+package moo;
+
+class Moo {
+	public static inline function moo(?string:String = "Have you mooed today?") {
+		return "\n         (__) \n        (oo) \n  /------\\/ \n / |    ||   \n*  /\\---/\\ \n   ~~   ~~   \n....\"" + string + "\"....";
+	}
+}
