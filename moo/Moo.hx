bd9e76aa2ac3969a14dcf47e60fd1e52a7a64a6e
sayofthelor <35895538+sayofthelor@users.noreply.github.com>
2022-03-29 14:51:18 -0400

    Update Moo.hx

--- moo/Moo.hx
+++ moo/Moo.hx
@@ -2,6 +2,6 @@ package moo;
 
 class Moo {
 	public static inline function moo(?string:String = "Have you mooed today?") {
-		return "\n         (__) \n        (oo) \n  /------\\/ \n / |    ||   \n*  /\\---/\\ \n   ~~   ~~   \n....\"" + string + "\"....";
+		return "\n        (__) \n        (oo) \n  /------\\/ \n / |    ||   \n*  /\\---/\\ \n   ~~   ~~   \n....\"" + string + "\"....";
 	}
 }
